27257e945736daa5ee95a496469fc2a36066dd4e
Federico Magliani <federico.magliani@gmail.com>
2017-12-07 11:14:03 +0100

    Solving 2 problems
    -require_flatten instead of input_shape
    -wrong url path of weights_no_top

--- vgg16_places_365.py
+++ vgg16_places_365.py
@@ -26,7 +26,7 @@ from keras.utils.data_utils import get_file
 from keras.utils import layer_utils
 
 WEIGHTS_PATH = 'https://github.com/GKalliatakis/Keras-VGG16-places365/releases/download/v0.1/vgg16-places365_weights_tf_dim_ordering_tf_kernels.h5'
-WEIGHTS_PATH_NO_TOP = 'https://github.com/GKalliatakis/Keras-VGG16-places365/releases/download/v0.1/vgg16-places365_weights_tf_dim_ordering_tf_kernels_notop.h5.h5'
+WEIGHTS_PATH_NO_TOP = 'https://github.com/GKalliatakis/Keras-VGG16-places365/releases/download/v0.1/vgg16-places365_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 
 def VGG16_Places365(include_top=True, weights='places',
@@ -96,7 +96,7 @@ def VGG16_Places365(include_top=True, weights='places',
                                       default_size=224,
                                       min_size=48,
                                       data_format=K.image_data_format(),
-                                      include_top=include_top)
+                                      require_flatten =include_top)
 
     if input_tensor is None:
         img_input = Input(shape=input_shape)
