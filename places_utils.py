63852be7aa9ff3b7de1d2af5155e0ea7556b1f3b
Grigorios Kalliatakis <gkalliatakis@yahoo.gr>
2018-01-18 14:33:37 +0000

    Keep only preprocess_input function

--- places_utils.py
+++ places_utils.py
@@ -1,157 +1,20 @@
-import json
-import warnings
-import numpy as np
 from keras import backend as K
-from keras.utils.data_utils import get_file
 
-CLASS_INDEX = None
-CLASS_INDEX_PATH = 'https://github.com/GKalliatakis/Keras-VGG16-places365/blob/master/places365_class_index.json'
+def preprocess_input(x, dim_ordering='default'):
+    if dim_ordering == 'default':
+        dim_ordering = K.image_dim_ordering()
+    assert dim_ordering in {'tf', 'th'}
 
-
-def decode_predictions(preds, top=5):
-    """Decodes the prediction of a Places model.
-    # Arguments
-        preds: Numpy tensor encoding a batch of predictions.
-        top: integer, how many top-guesses to return.
-    # Returns
-        A list of lists of top class prediction tuples
-        `(class_name, class_description, score)`.
-        One list of tuples per sample in batch input.
-    # Raises
-        ValueError: in case of invalid shape of the `pred` array
-            (must be 2D).
-    """
-    global CLASS_INDEX
-    if len(preds.shape) != 2 or preds.shape[1] != 365:
-        raise ValueError('`decode_predictions` expects '
-                         'a batch of predictions '
-                         '(i.e. a 2D array of shape (samples, 365)). '
-                         'Found array with shape: ' + str(preds.shape))
-    if CLASS_INDEX is None:
-        fpath = get_file('places365_class_index.json',
-                         CLASS_INDEX_PATH)
-        CLASS_INDEX = json.load(open(fpath))
-    results = []
-    for pred in preds:
-        top_indices = pred.argsort()[-top:][::-1]
-        result = [tuple(CLASS_INDEX[str(i)]) + (pred[i],) for i in top_indices]
-        result.sort(key=lambda x: x[2], reverse=True)
-        results.append(result)
-    return results
-
-
-def preprocess_input(x, data_format=None, mode='caffe'):
-    """Preprocesses a tensor encoding a batch of images.
-    # Arguments
-        x: input Numpy or symoblic tensor, 3D or 4D.
-        data_format: data format of the image tensor.
-        mode: One of "caffe", "tf".
-            - caffe: will convert the images from RGB to BGR,
-                then will zero-center each color channel with
-                respect to the ImageNet dataset,
-                without scaling.
-            - tf: will scale pixels between -1 and 1,
-                sample-wise.
-    # Returns
-        Preprocessed tensor.
-    """
-    if data_format is None:
-        data_format = K.image_data_format()
-    if data_format not in {'channels_first', 'channels_last'}:
-        raise ValueError('Unknown data_format ' + str(data_format))
-
-    if isinstance(x, np.ndarray):
-        return _preprocess_numpy_input(x, data_format=data_format, mode=mode)
+    if dim_ordering == 'th':
+        x[:, 0, :, :] -= 104.006
+        x[:, 1, :, :] -= 116.669
+        x[:, 2, :, :] -= 122.679
+        # 'RGB'->'BGR'
+        x = x[:, ::-1, :, :]
     else:
-        return _preprocess_symbolic_input(x, data_format=data_format,
-                                          mode=mode)
-
-
-def _preprocess_numpy_input(x, data_format, mode):
-    if mode == 'tf':
-        x /= 127.5
-        x -= 1.
-        return x
-
-    if mode == 'torch':
-        x /= 255.
-        mean = [0.485, 0.456, 0.406]
-        std = [0.229, 0.224, 0.225]
-    else:
-        if data_format == 'channels_first':
-            # 'RGB'->'BGR'
-            if x.ndim == 3:
-                x = x[::-1, ...]
-            else:
-                x = x[:, ::-1, ...]
-        else:
-            # 'RGB'->'BGR'
-            x = x[..., ::-1]
-        mean = [104.006, 116.669, 122.679]
-        std = None
-
-    # Zero-center by mean pixel
-    if data_format == 'channels_first':
-        if x.ndim == 3:
-            x[0, :, :] -= mean[0]
-            x[1, :, :] -= mean[1]
-            x[2, :, :] -= mean[2]
-            if std is not None:
-                x[0, :, :] /= std[0]
-                x[1, :, :] /= std[1]
-                x[2, :, :] /= std[2]
-        else:
-            x[:, 0, :, :] -= mean[0]
-            x[:, 1, :, :] -= mean[1]
-            x[:, 2, :, :] -= mean[2]
-            if std is not None:
-                x[:, 0, :, :] /= std[0]
-                x[:, 1, :, :] /= std[1]
-                x[:, 2, :, :] /= std[2]
-    else:
-        x[..., 0] -= mean[0]
-        x[..., 1] -= mean[1]
-        x[..., 2] -= mean[2]
-        if std is not None:
-            x[..., 0] /= std[0]
-            x[..., 1] /= std[1]
-            x[..., 2] /= std[2]
-    return x
-
-
-def _preprocess_symbolic_input(x, data_format, mode):
-    global _IMAGENET_MEAN
-
-    if mode == 'tf':
-        x /= 127.5
-        x -= 1.
-        return x
-
-    if mode == 'torch':
-        x /= 255.
-        mean = [0.485, 0.456, 0.406]
-        std = [0.229, 0.224, 0.225]
-    else:
-        if data_format == 'channels_first':
-            # 'RGB'->'BGR'
-            if K.ndim(x) == 3:
-                x = x[::-1, ...]
-            else:
-                x = x[:, ::-1, ...]
-        else:
-            # 'RGB'->'BGR'
-            x = x[..., ::-1]
-            mean = [104.006, 116.669, 122.679]
-        std = None
-
-    if _IMAGENET_MEAN is None:
-        _IMAGENET_MEAN = K.constant(-np.array(mean))
-
-    # Zero-center by mean pixel
-    if K.dtype(x) != K.dtype(_IMAGENET_MEAN):
-        x = K.bias_add(x, K.cast(_IMAGENET_MEAN, K.dtype(x)), data_format)
-    else:
-        x = K.bias_add(x, _IMAGENET_MEAN, data_format)
-    if std is not None:
-        x /= std
+        x[:, :, :, 0] -= 104.006
+        x[:, :, :, 1] -= 116.669
+        x[:, :, :, 2] -= 122.679
+        # 'RGB'->'BGR'
+        x = x[:, :, :, ::-1]
     return x
